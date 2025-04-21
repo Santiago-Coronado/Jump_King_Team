6a69d750db06a5dd583a00bacbc7202ee2ff288c
Antonio Pires <A01424547@tec.mx>
2025-04-21 12:19:15 -0600

    Tutorial de controles y trucate tables agregado

---
+++ Videojuego/sql/knightFalls-trucate.sql
@@ -0,0 +1,15 @@
+-- Desactivar temporalmente las restricciones de clave foránea
+use knightfalldb;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Truncar todas las tablas (eliminar todos los datos)
+TRUNCATE TABLE jugador;
+TRUNCATE TABLE partida;
+TRUNCATE TABLE partida_jugador;
+TRUNCATE TABLE usuario;
+
+
+-- Reactivar las restricciones de clave foránea
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- Ahora puedes ejecutar los nuevos INSERT
